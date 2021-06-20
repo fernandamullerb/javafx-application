1d58bfdb77cdda0cae904793928138d0f20f0177
Fernanda Müller Baldi <fernandamullerb@gmail.com>
2021-06-19 21:15:52 -0300

    Create: Validacoes de entradas

---
+++ src/main/Validacoes.java
@@ -0,0 +1,24 @@
+package main;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class Validacoes {
+    
+    public static void validarTipoEntrada(TextField txt) {
+        txt.textProperty().addListener((obs, oldValue, newValue) -> {
+            if (newValue != null && !newValue.matches("\\d*([\\.]\\d*)?")) {
+                    txt.setText(oldValue);
+            }
+        });
+    }
+    
+    public static boolean validarPreenchimentoNotas(ComboBox cbb, TextField txt1, TextField txt2) {      
+        return (cbb.getValue().equals("") || txt1.getText().isEmpty() || txt2.getText().isEmpty());
+   }
+
+    
+    
+    
+}
